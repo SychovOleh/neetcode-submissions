4a75cc6444c78b1e7c0a1b5e83740f69bbd6faec
Oleh Sychov <oleh.sychov@odido.nl>
2026-05-23 17:32:36 +0200

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        // ["1","2","+","3","*","4","-"]
+        // 1 2 + => 3
+        // 3 3 * => 9
+        // 9 4 - => 5
+        const s = [];
+        const o = {
+            '+': '+',
+            '-': '-',
+            '*': '*',
+            '/': '/',
+        }
+        for (let i = 0; i < tokens.length; i++) {
+            const t = tokens[i];
+            if (o[t]) {
+                const b = s.pop();
+                const a = s.pop();
+                let val;
+                if (o[t] === '+') {
+                    val = a + b;
+                } else if (o[t] === '-') {
+                    val = a - b;
+                } else if (o[t] === '*') {
+                    val = a * b;
+                } else {
+                    val = Math.trunc(a / b);
+                }
+                s.push(val);
+            } else {
+                s.push(Number(t));
+            }
+        }
+        return s[0];
+    }
+}
