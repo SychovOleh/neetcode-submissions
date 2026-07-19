79fe7dca3bd6ae61292ad5c08a4aa8f4f1c5a76c
Oleh Sychov <oleh.sychov@odido.nl>
2026-07-19 13:27:47 +0200

    Add: find-minimum-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-6.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        let l = 0;
+        let r = nums.length - 1;
+        
+        while (l < r) {
+            const m = Math.floor((l + r) / 2);    
+            const next = m + 1;
+            // if (nums[next] < nums[m]) {
+            //     return nums[next];
+            // }
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else if (nums[m] < nums[r]) {
+                r = m;
+            } else {
+                return nums[m];
+            }
+        }
+        return nums[l]
+    }
+}
