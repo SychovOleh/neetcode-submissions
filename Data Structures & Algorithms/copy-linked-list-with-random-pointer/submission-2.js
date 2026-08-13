681cced4f661e94fe3d2cac949e8d0919f6c8b07
Oleh Sychov <oleh.sychov@odido.nl>
2026-08-13 09:42:07 +0200

    Add: copy-linked-list-with-random-pointer - submission-2

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-2.js
@@ -0,0 +1,33 @@
+// class Node {
+//   constructor(val, next = null, random = null) {
+//       this.val = val;
+//       this.next = next;
+//       this.random = random;
+//   }
+// }
+
+class Solution {
+    /**
+     * @param {Node} head
+     * @return {Node}
+     */
+    copyRandomList(head) {
+        if (!head) return null;
+
+        const nodesMap = new Map();
+        let cur = head;
+        while (cur) {
+            const nodeCopy = new Node(cur.val);
+            nodesMap.set(cur, nodeCopy);
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur) {
+            const copy = nodesMap.get(cur);
+            copy.next = nodesMap.get(cur.next);
+            copy.random = nodesMap.get(cur.random);
+            cur = cur.next;
+        }
+        return nodesMap.get(head);
+    }
+}
