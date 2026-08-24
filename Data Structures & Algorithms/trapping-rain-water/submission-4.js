b55825b8621463be2ce6568fc1b926effa008d85
Oleh Sychov <oleh.sychov@odido.nl>
2026-08-24 19:58:12 +0200

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let total = 0;
+        let l = 0;
+        let prevL = l;
+        for (let r = 0; r < height.length; r++) {
+            if (height[l] <= height[r] || r === height.length - 1) {
+                while (l < r) {
+                    const min = Math.min(height[prevL], height[r]);
+                    const m3 = min - height[l];
+                    if (m3 > 0) {
+                        total += m3;
+                    }
+                    l++;
+                }
+                prevL = l;
+            }
+        }
+        return total;
+    }
+}
