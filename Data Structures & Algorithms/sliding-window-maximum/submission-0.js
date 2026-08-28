dee5fc77074d662ca9d5bc681eb8db744fc70d19
Oleh Sychov <oleh.sychov@odido.nl>
2026-08-28 08:47:36 +0200

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        const max = new Array();
+        const q = new Deque();
+        for (let r = 0; r < nums.length; r++) {
+            while (q.size() > 0 && nums[r] >= nums[q.back()]) {
+                q.popBack();
+            }
+            if (q.front() && q.front() === r - k) {
+                q.popFront();
+            }
+            q.pushBack(r)
+            if (r >= k - 1) {
+                max.push(nums[q.front()]);
+            }
+        }
+        return max;
+    }
+}
+ 
