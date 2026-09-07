e492809af01ebc0271df8519891b4c553530d6d4
Oleh Sychov <oleh.sychov@odido.nl>
2026-09-07 08:15:02 +0200

    Add: online-stock-span - submission-2

---
+++ Data Structures & Algorithms/online-stock-span/submission-2.js
@@ -0,0 +1,24 @@
+class StockSpanner {
+    constructor() {
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} price
+     * @return {number}
+     */
+    next(price) {
+        let count = 1;
+        while(this.stack.at(-1) && this.stack.at(-1)[0] <= price) {
+            count += this.stack.pop()[1];
+        }
+        this.stack.push([price, count]);
+        return count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
