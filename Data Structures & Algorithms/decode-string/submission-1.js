497d7607fe46be073815e6c28899488652ed1700
Oleh Sychov <oleh.sychov@odido.nl>
2026-09-12 08:11:28 +0200

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    decodeString(s) {
+        let chars = [];
+        for (let i = 0; i < s.length; i++) {
+            let c = s[i];
+            if (c === ']') {
+                let str = '';
+                c = chars.at(-1);
+                while (c !== '[') {
+                    c = chars.pop();
+                    if (c !== '[') {
+                        str = c + str;
+                    }
+                }
+                let num = '';
+                c = chars.at(-1);
+                while (c !== undefined && !isNaN(parseInt(c))) {
+                    chars.pop();
+                    num = c + num;
+                    c = chars.at(-1);
+                }
+
+                for (let count = parseInt(num); count > 0; count--) {
+                    chars.push(str);
+                }
+                continue;
+            }
+            chars.push(c);
+        }
+
+        return chars.join('');
+    }
+}
