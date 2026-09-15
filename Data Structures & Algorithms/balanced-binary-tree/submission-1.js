398efbc4a67e8309d839dc67cc83c066f7dbca81
Oleh Sychov <oleh.sychov@odido.nl>
2026-09-15 19:33:42 +0200

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        if (!root) return true;
+
+        const countDepth = (node, n) => {
+            if (!node.left && !node.right) {
+                return n
+            }
+            const l = node.left ? countDepth(node.left, n + 1) : n;
+            const r = node.right ? countDepth(node.right, n + 1) : n;
+            return Math.max(l, r);
+        }
+        const l = root.left ? countDepth(root.left, 1) : 0;
+        const r = root.right ? countDepth(root.right, 1) : 0;
+        return Math.abs(l - r) <= 1 && this.isBalanced(root.left) && this.isBalanced(root.right);
+    }
+}
