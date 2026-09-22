0dae364efab72b62572ede30247e4ce750ef0502
Oleh Sychov <oleh.sychov@odido.nl>
2026-09-22 09:45:01 +0200

    Add: find-k-closest-elements - submission-7

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-7.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @param {number} k
+     * @param {number} x
+     * @return {number[]}
+     */
+    findClosestElements(arr, k, x) {
+        let l = 0;
+        let r = arr.length - 1;
+        while (l < r) {
+            const m = Math.floor((l + r) / 2);
+            // const diffL = Math.abs(arr[m - 1] - x);
+            const diffR = Math.abs(arr[m + 1] - x);
+            const diffM = Math.abs(arr[m] - x);
+            if (arr[m] < x && (arr[m + 1] < x || diffM > diffR)) {
+                    l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+
+        while (r - l + 1 < k) {
+            const diffL = Math.abs(arr[l - 1] - x);
+            const diffR = Math.abs(arr[r + 1] - x);
+            if (r === arr.length - 1) {
+                l--;
+            } else if (l === 0) {
+                r++;
+            } else if (diffL > diffR) {
+                r++;
+            } else {
+                l--;
+            }
+        }
+
+        return arr.slice(l, r + 1);
+    }
+}
